71891f4ec274954d790c90383ee4f1cb13179bc7
AHN JAEWON <dsfs3975@naver.com>
2024-06-18 19:13:12 +0900

    [Bronze IV] Title: 주사위 세개, Time: 184 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        if (a == b && b == c) {
+            System.out.println(10000 + a * 1000);
+        } else if (a == b || a == c || b == c) {
+            System.out.println(1000 + (a == b ? a : c) * 100);
+        } else {
+            System.out.println(Math.max(Math.max(a, b), c) * 100);
+        }
+    }
+}
