f6dc063644c5a42a9b2749ddfc8ddcd422454c80
AHN JAEWON <dsfs3975@naver.com>
2024-06-20 22:48:46 +0900

    [Bronze III] Title: 최소, 최대, Time: 1620 ms, Memory: 318484 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] numbers = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        int min = numbers[0];
+        int max = numbers[0];
+
+        for (int i = 1; i < N; i++) {
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+        sc.close();
+        System.out.println(min + " " + max);
+    }
+}
