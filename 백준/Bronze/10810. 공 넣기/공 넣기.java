ea5388989412435a4574a9b5f315ecf68e99ac4c
AHN JAEWON <dsfs3975@naver.com>
2024-06-20 22:51:52 +0900

    [Bronze III] Title: 공 넣기, Time: 124 ms, Memory: 15900 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] array = new int[N+1];
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int I = Integer.parseInt(st.nextToken());
+            int J = Integer.parseInt(st.nextToken());
+            int K = Integer.parseInt(st.nextToken());
+
+            for (int j = I; j <= J; j++) {
+                array[j] = K;
+            }
+        }
+
+        for (int i = 1; i <= N; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
