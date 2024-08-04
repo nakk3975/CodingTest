8a5610baa519e7670009adb0c1b0bc14a01606d6
AHN JAEWON <dsfs3975@naver.com>
2024-08-04 16:21:19 +0900

    [level 1] Title: 둘만의 암호, Time: 16.98 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(String s, String skip, int index) {
+        String answer = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            for (int j = 0; j < index; j++) {
+                c += 1;
+                //a97 z122
+                // z 넘어갈 시 다시 a로
+                if (c > 'z') {
+                    c -= 26;
+                }
+                if (skip.contains(String.valueOf(c))) {
+                    j--;
+                }
+            }
+            answer += c;
+        }
+        return answer;
+    }
+}
