6c5917c9a79873c40b731d926da13ed1f2bee567
AHN JAEWON <dsfs3975@naver.com>
2024-09-11 21:15:37 +0900

    [level 0] Title: 이어 붙인 수, Time: 19.74 ms, Memory: 83.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        String even = "";
+        String odd = "";
+        
+        for(int i = 0; i < num_list.length; i++) {
+            if(num_list[i] % 2 == 0) {
+                even += num_list[i] + "";
+            } else {
+                odd += num_list[i] + "";
+            }
+        }
+        
+        int evenNum = Integer.parseInt(even);
+        int oddNum = Integer.parseInt(odd);
+        
+        answer = evenNum + oddNum;
+        
+        return answer;
+    }
+}
